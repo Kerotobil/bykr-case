2c3d70b50832832183e490d2492f50deb9bd71c6
Kerem Gumus <keremgumus49@gmail.com>
2022-10-28 00:13:45 +0300

    Add Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Logo } from './Logo';
+import { Bars3Icon } from '@heroicons/react/24/solid';
+
+export const Navbar = () => {
+  const menuList = ['About Us', 'What We Do', 'Our work', 'Blog', 'Say hi'];
+  return (
+    <div className="flex justify-between px-4 items-center max-w-7xl">
+      <Logo />
+      <ul className="flex justify-between w-2/4 text-white">
+        {menuList.map((item, index) => (
+          <li key={index}>
+            <span> {item}</span>
+          </li>
+        ))}
+      </ul>
+      <Bars3Icon className="w-6 text-white" />
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
-import styles from '../styles/Home.module.css'
+import { Navbar } from '../components/Navbar';
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-800 mx-auto">
+      <div className="max-w-7xl mx-auto">
+        <Navbar />
+      </div>
     </div>
-  )
+  );
 }
