35526fec1107ef009f4d029a27ed86444f9d107b
Kerem Gumus <keremgumus49@gmail.com>
2022-10-28 21:17:20 +0300

    Add Inter font

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { SecondLayout } from '../components/Home/SecondLayout';
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mainBg mx-auto text-white">
+    <div className="max-w-7xl mainBg mx-auto text-white font-inter">
       <div className=" h-screen mx-auto pt-10">
         <Navbar />
         <div className="mt-16">
