dbdc85c2ef2d785e6975b26ca1197e558b24ce1c
Kerem Gumus <keremgumus49@gmail.com>
2022-10-29 12:10:16 +0300

    Edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        '1/2xl': '1440px',
+      },
+      colors: {
+        'custom': '#353F5B',
+      },
       fontFamily: {
         inter: ['Inter'],
       },
