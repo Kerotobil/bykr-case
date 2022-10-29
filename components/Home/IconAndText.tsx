aefa7ce6259073df99fd9a9df157c32504472b77
Kerem Gumus <keremgumus49@gmail.com>
2022-10-29 20:36:40 +0300

    Edit third layout for responsive

--- components/Home/IconAndText.tsx
+++ components/Home/IconAndText.tsx
@@ -6,23 +6,20 @@ export const IconAndText = (props: Props) => {
   return (
     <div className="flex space-x-5">
       <div>
-        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M27.5 13.8501V15.0001C27.4985 17.6956 26.6256 20.3184 25.0117 22.4774C23.3977 24.6363 21.1291 26.2157 18.5442 26.98C15.9593 27.7442 13.1966 27.6525 10.6681 26.7183C8.1396 25.7842 5.98082 24.0577 4.5137 21.7964C3.04658 19.5351 2.34974 16.8602 2.5271 14.1705C2.70445 11.4808 3.74651 8.92048 5.49785 6.87142C7.24919 4.82235 9.61598 3.3943 12.2452 2.80026C14.8745 2.20621 17.6253 2.478 20.0875 3.57508"
             stroke="white"
             strokeWidth="2"
             strokeLinecap="round"
+            className="stroke-yellow-500 md:stroke-white"
           />
-          <path
-            d="M27.5 5L15 17.5125L11.25 13.7625"
-            stroke="white"
-            strokeWidth="2"
-          />
+          <path d="M27.5 5L15 17.5125L11.25 13.7625" className="stroke-yellow-500 md:stroke-white" strokeWidth="2" />
         </svg>
       </div>
       <div>
-        <h1 className="text-2xl font-bold"> {props.title} </h1>
-        <p className="mt-4 text-base text-zinc-200 max-w-md">{props.text} </p>
+        <h1 className="text-xl md:text-2xl font-bold"> {props.title} </h1>
+        <p className="mt-4 md:mt-6 text-base text-zinc-200 max-w-md">{props.text} </p>
       </div>
     </div>
   );
--- components/Home/ThirdLayout.tsx
+++ components/Home/ThirdLayout.tsx
@@ -4,13 +4,15 @@ import { IconAndText } from './IconAndText';
 export const ThirdLayout = () => {
   return (
     <div className="relative">
-      <div className="flex relative pt-24 pb-20 px-8 z-20">
-        <div className="z-20">
+      <div className="flex flex-col-reverse md:flex-row relative pt-24 pb-20 px-8 z-20">
+        <div className="relative z-20 mt-10 md:mt-0">
           <Image src="/Rectangle4.png" alt="" width={650} height={120} className="z-40" />
+          <div className="md:hidden absolute right-0 -bottom-28 bg-yellow-500 h-96 w-96 -z-10"></div>
+          <div className="md:hidden absolute left-0 -bottom-28 bg-yellow-500 h-96 w-96 -z-10"></div>
         </div>
-        <div className="ml-14 max-w-xl pt-12 z-20">
-          <h1 className="text-5xl font-bold">Our Solution Approach</h1>
-          <div className="space-y-6 mt-12">
+        <div className="md:ml-14 max-w-xl pt-12 z-20">
+          <h1 className="text-2xl md:text-5xl font-bold">Our Solution Approach</h1>
+          <div className="space-y-12 mt-12">
             <IconAndText
               title="Data Analysis with problem factor"
               text="Delivering the spatial expertise of the largest, most sophisticated performance marketing agencies, while providing intimate, boutique-like support."
@@ -26,7 +28,7 @@ export const ThirdLayout = () => {
           </div>
         </div>
       </div>
-      <div className="absolute top-0 z-10 h-full w-full flex">
+      <div className="hidden md:flex absolute top-0 z-10 h-full w-full">
         <div className="bg-yellow-500 w-5/12 h-full "></div>
         <div className="bg-custom w-7/12 h-full"></div>
       </div>
