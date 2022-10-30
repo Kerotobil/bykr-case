6e4255e18b4a2106cf779411434635163d8ef978
Kerem Gumus <keremgumus49@gmail.com>
2022-10-30 16:49:18 +0300

    Add country type

---
+++ types.ts
@@ -0,0 +1,14 @@
+export interface country {
+  area: number;
+  borders: string[];
+  capital: string[];
+  cca3: string;
+  fifa: string;
+  flag: string;
+  independent: boolean;
+  maps: { googleMaps: string };
+  name: { common: string; official: string };
+  population: number;
+  region: string;
+  subregion: string;
+}
